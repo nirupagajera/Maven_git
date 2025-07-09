c756e6378edf427aac72d66eb2bf09c906db4a11
nirupagajera <nirupagajera9999@gmail.com>
2025-07-09 15:42:51 +0530

    First Commit

---
+++ git/src/main/java/git/Browser_Open.java
@@ -0,0 +1,21 @@
+package git;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Browser_Open {
+		
+	public WebDriver driver;
+	
+	@Test(priority = 1)
+	public void browser_open() throws Exception
+	{
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("http://krishaweb.com/");
+		
+	}
+}
